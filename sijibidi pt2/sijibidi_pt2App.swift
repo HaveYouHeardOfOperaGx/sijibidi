6c98ccff7439c7bcaf08f707244e9e95aa5ffcf3
HaveYouHeardOfOperaGx <teo_yu_jie_jordan@s2024.ssts.edu.sg>
2024-09-09 16:04:30 +0800

    Initial Commit

---
+++ sijibidi pt2/sijibidi_pt2App.swift
@@ -0,0 +1,17 @@
+//
+//  sijibidi_pt2App.swift
+//  sijibidi pt2
+//
+//  Created by Jordan on 9/9/24.
+//
+
+import SwiftUI
+
+@main
+struct sijibidi_pt2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
